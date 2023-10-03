2eeb64e5c2d3ddc1de551150f7e295eca47e9ea7
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-02 20:51:49 -0600

    Adding verbosity

--- src/tcp_client.c
+++ src/tcp_client.c
@@ -169,8 +169,11 @@ int tcp_client_receive_response(int sockfd, int (*handle_response)(char *)) {
 
     // Fill the buffer with info from the server
     do {
-        if (verbose_flag)
-            log_log(LOG_DEBUG, __FILE__, __LINE__, "Bytes read: %d", bytesReceived);
+        if (verbose_flag) {
+            char *dummyBuf = buf;
+            dummyBuf[strlen(dummyBuf)] = '\0';
+            log_log(LOG_DEBUG, __FILE__, __LINE__, "Bytes read: %d, buffer = %s", bytesReceived, dummyBuf);
+        }
         bytesReceived = recv(sockfd, buf + bytesReceived, TCP_CLIENT_MAX_INPUT_SIZE - bytesReceived - 1, 0);
         if (bytesReceived > 0) bytesReceived += bytesReceived;
         if (verbose_flag)
