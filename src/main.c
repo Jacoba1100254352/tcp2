43d45b3b7e7ff9488deff96b855d83ade7103244
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-03 22:27:41 -0600

    Adding verbosity

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 
 int verbose_flag = 0;  // Global variable for the verbose flag
 
+// Global counters for sent and received messages
+static size_t messages_sent = 0;
+static size_t messages_received = 0;
+
 // Function to print usage instructions when --help is used or invalid arguments are provided
 static void printHelpOption(char *argv[]) {
     fprintf(stderr, "Usage: %s [--help] [-v] [-h HOST] [-p PORT] FILE\n"
@@ -42,7 +46,10 @@ int handle_response(char *response) {
         ptr += len;
     }
 
-    // If we've reached this point, all messages in the buffer have been processed.
+    messages_received++;
+    if (messages_sent > messages_received) {
+        return 0;
+    }
     return EXIT_SUCCESS;
 }
 
@@ -51,23 +58,26 @@ int main(int argc, char *argv[]) {
             .host = TCP_CLIENT_DEFAULT_HOST,
             .port = TCP_CLIENT_DEFAULT_PORT
     };
+
     if (tcp_client_parse_arguments(argc, argv, &config)) {
         printHelpOption(argv);
         exit(EXIT_FAILURE);
     }
 
     int sockfd = tcp_client_connect(config);
-    if (sockfd == TCP_CLIENT_BAD_SOCKET) exit(EXIT_FAILURE);
+    if (sockfd == TCP_CLIENT_BAD_SOCKET) {
+        log_warn("Unable to connect to a socket, exiting program");
+        exit(EXIT_FAILURE);
+    }
 
     if (verbose_flag)
         log_log(LOG_DEBUG, __FILE__, __LINE__, "Connected to %s:%s", config.host, config.port);
 
-
-    // Open the specified file or use stdin
     FILE *fp;
-    if (strcmp(config.file, "-") == 0)
+    if (strcmp(config.file, "-") == 0) {
         fp = stdin;
-    else if (!(fp = tcp_client_open_file(config.file))) {
+    } else if (!(fp = tcp_client_open_file(config.file))) {
+        log_error("There was an error trying to open the file.");
         tcp_client_close(sockfd);
         return EXIT_FAILURE;
     }
@@ -82,13 +92,15 @@ int main(int argc, char *argv[]) {
             tcp_client_close(sockfd);
             return EXIT_FAILURE;
         }
+        messages_sent++;
         free(action);
         free(message);
     }
+
+    log_info("Messages sent: %zu, messages received: %zu.", messages_sent, messages_received);
 
     if (fp != stdin)
         tcp_client_close_file(fp);
 
     exit((tcp_client_receive_response(sockfd, handle_response) || tcp_client_close(sockfd)) ? EXIT_FAILURE : EXIT_SUCCESS);
 }
-
